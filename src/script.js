515860727d0794957cca664dec5ddafa86d4b49a
Ben Bowles <benjamin.d.bowles@gmail.com>
2023-07-06 13:35:04 -0500

    mouse cursor

--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ import { LoadImages, LoadAudio } from "./scripts/loading.js";
 const images = {
   // name, path
   red_arrow: "./assets/images/red_arrow.png",
+  cursor: "./assets/images/cursor.png",
 };
 
 const audio = {
@@ -20,12 +21,21 @@ let cnv = new canvas("canvas");
 cnv.resize();
 
 const arrow = new gameobject(50, 50, 32, 32, images.red_arrow, true);
+const cursor = new gameobject(0, 0, 32, 32, images.cursor, false);
+
+document.addEventListener("mousemove", (event) => {
+  let pos = cnv.mousePosition(event);
+  cursor.setPos(pos);
+});
 
 const gameloop = setInterval(async () => {
   cnv.resetContext();
   cnv.clear();
   cnv.background();
-  cnv.drawGrid();
+  //cnv.drawGrid();
+
+  //mouse cursor
+  cursor.draw(cnv.context);
 
   // Your code goes here
   arrow.draw(cnv.context);
--- src/scripts/gameobject.js
+++ src/scripts/gameobject.js
@@ -1,11 +1,15 @@
 class gameobject {
   constructor(x = 0, y = 0, width, height, image_path, hitbox_shown = false) {
-    this.x = x;
-    this.y = y;
+    this.pos = { x: x, y: y };
     this.width = width;
     this.height = height;
     this.image_path = image_path;
     this.hitbox_shown = hitbox_shown;
+  }
+
+  setPos({ x, y }) {
+    this.pos.x = x;
+    this.pos.y = y;
   }
 
   draw(context) {
@@ -13,8 +17,8 @@ class gameobject {
     image.src = this.image_path;
 
     let pos = {
-      x: this.x - this.width / 2,
-      y: this.y - this.height / 2,
+      x: this.pos.x - this.width / 2,
+      y: this.pos.y - this.height / 2,
     };
 
     context.drawImage(image, pos.x, pos.y, this.width, this.height);
@@ -27,8 +31,8 @@ class gameobject {
 
   collisionWithPoint(x, y) {
     let pos = {
-      x: this.x - this.width / 2,
-      y: this.y - this.height / 2,
+      x: this.pos.x - this.width / 2,
+      y: this.pos.y - this.height / 2,
     };
 
     if (
